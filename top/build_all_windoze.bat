53fca0748b20eeb778e1d591885647ddc952e69a
johnt <john.t.taylor@gmail.com>
2023-06-19 08:22:30 -0400

    wip

--- top/build_all_windoze.bat
+++ top/build_all_windoze.bat
@@ -49,7 +49,7 @@ IF ERRORLEVEL 1 EXIT /b 1
 IF ERRORLEVEL 1 EXIT /b 1
 
 :: Generate code coverage metrics
-%_TOOLS%\chuck.py -vt --dir mingw_w64 --match tca.py rpt --xml coverage.xml 
+%_TOOLS%\chuck.py -v --dir mingw_w64 --match tca.py rpt --xml coverage.xml 
 IF ERRORLEVEL 1 EXIT /b 1
 
 ::
