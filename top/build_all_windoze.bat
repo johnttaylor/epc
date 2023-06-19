34e829b02477a1ec7325137b34d103ea0e5979ef
johnt <john.t.taylor@gmail.com>
2023-06-19 08:04:44 -0400

    wip

--- top/build_all_windoze.bat
+++ top/build_all_windoze.bat
@@ -17,33 +17,33 @@ echo:BUILD: BUILD_NUMBER=%BUILD_NUMBER%, BRANCH=%BUILD_BRANCH%
 echo:
 
 :: Run Doxygen first 
-cd %_TOPDIR%
-run_doxygen.py %BUILD_NUMBER% %BUILD_BRANCH% 
-IF ERRORLEVEL 1 EXIT /b 1
+::cd %_TOPDIR%
+::run_doxygen.py %BUILD_NUMBER% %BUILD_BRANCH% 
+::IF ERRORLEVEL 1 EXIT /b 1
 
 ::
 :: Build Mingw projects (just the Win32 builds)
 ::
-call %_TOPDIR%..\env.bat 3
-@echo on
-
-:: Build NON-unit-test projects
-cd %_TOPDIR%..\projects
-%_TOOLS%\bob.py -v4 mingw_w64 -c --bldtime -b win32 --bldnum %BUILD_NUMBER%
-IF ERRORLEVEL 1 EXIT /b 1
-
-:: Build unit test projects
-cd %_TOPDIR%..\tests
-%_TOOLS%\bob.py -v4 mingw_w64 -c --bldtime -b win32  --bldnum %BUILD_NUMBER%
-IF ERRORLEVEL 1 EXIT /b 1
+::call %_TOPDIR%..\env.bat 3
+::@echo on
+::
+:::: Build NON-unit-test projects
+::cd %_TOPDIR%..\projects
+::%_TOOLS%\bob.py -v4 mingw_w64 -c --bldtime -b win32 --bldnum %BUILD_NUMBER%
+::IF ERRORLEVEL 1 EXIT /b 1
+::
+:::: Build unit test projects
+::cd %_TOPDIR%..\tests
+::%_TOOLS%\bob.py -v4 mingw_w64 -c --bldtime -b win32  --bldnum %BUILD_NUMBER%
+::IF ERRORLEVEL 1 EXIT /b 1
 
 :: Run unit tests
 cd %_TOPDIR%..\tests
-%_TOOLS%\chuck.py -vt --match a.exe --dir mingw_w64
+%_TOOLS%\chuck.py -v --match a.exe --dir mingw_w64
 IF ERRORLEVEL 1 EXIT /b 1
 %_TOOLS%\chuck.py -v --match aa.exe --dir mingw_w64
 IF ERRORLEVEL 1 EXIT /b 1
-%_TOOLS%\chuck.py -vt --match a.py --dir mingw_w64
+%_TOOLS%\chuck.py -v --match a.py --dir mingw_w64
 IF ERRORLEVEL 1 EXIT /b 1
 %_TOOLS%\chuck.py -v --match aa.py --dir mingw_w64
 IF ERRORLEVEL 1 EXIT /b 1
@@ -55,26 +55,26 @@ IF ERRORLEVEL 1 EXIT /b 1
 ::
 :: Build Visual Studio projects (just the win32 builds)
 ::
-call %_TOPDIR%..\env.bat 1
-@echo on
-
-:: Build NON-unit-test projects 
-cd %_TOPDIR%..\projects
-%_TOOLS%\bob.py -v4 vc12 -c --bldtime -b win32 --bldnum %BUILD_NUMBER%
-IF ERRORLEVEL 1 EXIT /b 1
-
-:: Build unit test projects
-cd %_TOPDIR%..\tests
-%_TOOLS%\bob.py -v4 vc12 --bldtime --clean-all -b win32 --bldnum %BUILD_NUMBER%
-IF ERRORLEVEL 1 EXIT /b 1
+::call %_TOPDIR%..\env.bat 1
+::@echo on
+::
+:::: Build NON-unit-test projects 
+::cd %_TOPDIR%..\projects
+::%_TOOLS%\bob.py -v4 vc12 -c --bldtime -b win32 --bldnum %BUILD_NUMBER%
+::IF ERRORLEVEL 1 EXIT /b 1
+::
+:::: Build unit test projects
+::cd %_TOPDIR%..\tests
+::%_TOOLS%\bob.py -v4 vc12 --bldtime --clean-all -b win32 --bldnum %BUILD_NUMBER%
+::IF ERRORLEVEL 1 EXIT /b 1
 
 :: Run unit tests
 cd %_TOPDIR%\..\tests
-%_TOOLS%\chuck.py -vt --match a.exe --dir vc12
+%_TOOLS%\chuck.py -v --match a.exe --dir vc12
 IF ERRORLEVEL 1 EXIT /b 1
 %_TOOLS%\chuck.py -v --match aa.exe --dir vc12
 IF ERRORLEVEL 1 EXIT /b 1
-%_TOOLS%\chuck.py -vt --match a.py --dir vc12
+%_TOOLS%\chuck.py -v --match a.py --dir vc12
 IF ERRORLEVEL 1 EXIT /b 1
 %_TOOLS%\chuck.py -v --match aa.py --dir vc12
 IF ERRORLEVEL 1 EXIT /b 1
