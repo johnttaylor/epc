9c4f4a28b6ab57180fa9bb6f243f92da76a1bd58
johnt <john.t.taylor@gmail.com>
2023-06-19 08:48:37 -0400

    wip

--- top/build_all_windoze.bat
+++ top/build_all_windoze.bat
@@ -7,6 +7,7 @@
 
 set _TOPDIR=%~dp0
 set _TOOLS=%_TOPDIR%..\xsrc\nqbp2\other
+set _ROOT=%_TOPDIR%..
 
 :: Set Build info (and force build number to zero for "non-official" builds)
 set BUILD_NUMBER=%1
@@ -24,21 +25,21 @@ echo:
 ::
 :: Build Mingw projects (just the Win32 builds)
 ::
-call %_TOPDIR%..\env.bat 3
+call %_ROOT%\env.bat 3
 @echo on
 
 :: Build NON-unit-test projects
-cd %_TOPDIR%..\projects
+cd %_ROOT%\projects
 %_TOOLS%\bob.py -v4 mingw_w64 -c --bldtime -b win32 --bldnum %BUILD_NUMBER%
 IF ERRORLEVEL 1 EXIT /b 1
 
 :: Build unit test projects
-cd %_TOPDIR%..\tests
+cd %_ROOT%\tests
 %_TOOLS%\bob.py -v4 mingw_w64 -c --bldtime -b win32  --bldnum %BUILD_NUMBER%
 IF ERRORLEVEL 1 EXIT /b 1
 
 :: Run unit tests
-cd %_TOPDIR%..\tests
+cd %_ROOT%\tests
 %_TOOLS%\chuck.py -v --match a.exe --dir mingw_w64
 IF ERRORLEVEL 1 EXIT /b 1
 %_TOOLS%\chuck.py -v --match aa.exe --dir mingw_w64
@@ -55,21 +56,21 @@ IF ERRORLEVEL 1 EXIT /b 1
 ::
 :: Build Visual Studio projects (just the win32 builds)
 ::
-call %_TOPDIR%..\env.bat 1
+call %_ROOT%\env.bat 1
 @echo on
 
 :: Build NON-unit-test projects 
-cd %_TOPDIR%..\projects      
+cd %_ROOT%\projects      
 %_TOOLS%\bob.py -v4 vc12 -c --bldtime -b win32 --bldnum %BUILD_NUMBER%
 IF ERRORLEVEL 1 EXIT /b 1
 
 :: Build unit test projects
-cd %_TOPDIR%..\tests
+cd %_ROOT%\tests
 %_TOOLS%\bob.py -v4 vc12 --bldtime --clean-all -b win32 --bldnum %BUILD_NUMBER%
 IF ERRORLEVEL 1 EXIT /b 1
 
 :: Run unit tests
-cd %_TOPDIR%\..\tests
+cd %_ROOT%\tests
 %_TOOLS%\chuck.py -v --match a.exe --dir vc12
 IF ERRORLEVEL 1 EXIT /b 1
 %_TOOLS%\chuck.py -v --match aa.exe --dir vc12
@@ -83,18 +84,18 @@ IF ERRORLEVEL 1 EXIT /b 1
 :: Build STM32 Target projects
 ::
 
-call %_TOPDIR%..\env.bat 5
+call %_ROOT%\env.bat 5
 @echo on
 
 :: Build NON-unit-test projects
 
-cd %_TOPDIR%..\projects
+cd %_ROOT%\projects
 %_TOOLS%\bob.py -v4 --p2 windows gcc-arm --bldtime -c --bld-all --bldnum %BUILD_NUMBER%
 IF ERRORLEVEL 1 EXIT /b 1
 
 :: Build unit test projects
 
-cd %_TOPDIR%..\tests
+cd %_ROOT%\tests
 %_TOOLS%\bob.py -v4 --p2 windows gcc-arm --bldtime -c --bld-all --bldnum %BUILD_NUMBER%
 IF ERRORLEVEL 1 EXIT /b 1
 
