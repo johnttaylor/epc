72902a6dc9b6bdc4e9332a1ec07c146db55dc609
johnt <john.t.taylor@gmail.com>
2023-10-28 17:23:40 -0400

    add missing files. Remove 'old' Adafruit BSP

---
+++ src/Driver/DIO/Arduino/mappings_.h
@@ -0,0 +1,24 @@
+#ifndef Driver_DIO_Ardunino_mapping_h_
+#define Driver_DIO_Ardunino_mapping_h_
+/*-----------------------------------------------------------------------------
+* This file is part of the Colony.Core Project.  The Colony.Core Project is an
+* open source project with a BSD type of licensing agreement.  See the license
+* agreement (license.txt) in the top/ directory or on the Internet at
+* http://integerfox.com/colony.core/license.txt
+*
+* Copyright (c) 2014-2023  John T. Taylor
+*
+* Redistributions of the source code must retain the above copyright notice.
+*----------------------------------------------------------------------------*/
+/** @file */
+
+/// Platform mapping - for Arduino this is the PIN number
+#define DriverDioOutPinConfig_T_MAP         int
+
+/// Platform mapping - for Arduino this is the PIN number
+#define DriverDioPwmConfig_T_MAP            int
+
+
+
+/*--------------------------------------------------------------------------*/
+#endif  // end header latch
