07e477699bc7ecd7eaf65198b043f55b09d8657a
johnt <john.t.taylor@gmail.com>
2023-12-17 13:27:03 -0500

    wip

--- src/Ajax/Ui/Home/Screen.cpp
+++ src/Ajax/Ui/Home/Screen.cpp
@@ -15,24 +15,25 @@
 
 using namespace Ajax::Ui::Home;
 
-#define X0_TITLE                    90
-#define Y0_TITLE                    4
-
 
 #define TEXT_HEIGHT                 18
 
 #define COLUMN0_X0                  10
-#define COLUMN1_X0                  (COLUMN0_X0+50+8)
+#define COLUMN1_X0                  (COLUMN0_X0+60+8)
+#define COLUMN2_X0                  (COLUMN0_X1+40+8)
+#define COLUMN3_X0                  (COLUMN0_X1+40+8)
 
-#define ROW0_Y0                     (Y0_TITLE+TEXT_HEIGHT+TEXT_HEIGHT/2)
+
+#define ROW0_Y0                     (TEXT_HEIGHT/2)
 #define ROW1_Y0                     (ROW0_Y0+ROW_HEIGHT)
-#define ROW2_Y0                     (ROW1_Y0+ROW_HEIGHT)
+#define ROW2_Y0                     (ROW1_Y0+(2*ROW_HEIGHT))
 #define ROW3_Y0                     (ROW2_Y0+ROW_HEIGHT)
 #define ROW_HEIGHT                  (TEXT_HEIGHT+8)
 
 
-#define SET_PEN_BACKGROUND()        m_graphics.set_pen( 0, 0, 0 );      // black
-#define SET_PEN_TEXT()              m_graphics.set_pen( 0, 255, 0  );   // green
+#define SET_PEN_BACKGROUND()        m_graphics.set_pen( 0, 0, 0 );          // black
+#define SET_PEN_STATIC_TEXT()       m_graphics.set_pen( 255, 255, 255  );   // white
+#define SET_PEN_DYNAMIC_TEXT()      m_graphics.set_pen( 255, 255, 0  );     // Yellow
 
 ///////////////////////////
 Screen::Screen( Ajax::ScreenMgr::Navigation&    screenMgr,
@@ -53,12 +54,12 @@ void Screen::exit( Cpl::System::ElapsedTime::Precision_T currentElapsedTime ) no
 }
 void Screen::sleep( Cpl::System::ElapsedTime::Precision_T currentElapsedTime ) noexcept
 {
-    // TODO
+    // nothing needed
 }
 
 void Screen::wake( Cpl::System::ElapsedTime::Precision_T currentElapsedTime ) noexcept
 {
-    // TODO
+    // nothing needed
 }
 
 void Screen::dispatch( AjaxScreenMgrEvent_T event, Cpl::System::ElapsedTime::Precision_T currentElapsedTime ) noexcept
@@ -73,7 +74,7 @@ void Screen::dispatch( AjaxScreenMgrEvent_T event, Cpl::System::ElapsedTime::Pre
 
 void Screen::tick( Cpl::System::ElapsedTime::Precision_T currentElapsedTime ) noexcept
 {
-    // TODO
+    // nothing needed
 }
 
 bool Screen::refresh( Cpl::System::ElapsedTime::Precision_T currentElapsedTime ) noexcept
@@ -83,5 +84,15 @@ bool Screen::refresh( Cpl::System::ElapsedTime::Precision_T currentElapsedTime )
 
     // fill the screen with the current pen colour
     m_graphics.clear();
+
+    // Model Number
+    SET_PEN_TEXT();
+    Cpl::Text::FString<OPTION_AJAX_MAX_MODEL_LENGTH> tmpModel="<unknown>";
+    mp::modelNumber.read( tmpModel );
+    m_graphics.set_font( &font8 );
+    m_graphics.text( "model:", pimoroni::Point( COLUMN0_X0, ROW0_Y0 ), 240 );
+    m_graphics.text( tmpModel.getString(), pimoroni::Point( COLUMN1_X0, ROW0_Y0 ), 240 );
+
+    
     return true;
 }
