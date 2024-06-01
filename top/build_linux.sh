ff0dd9fd76a5eb0fa361f099c1ce216ab8f6856c
John Taylor <john.t.taylor@gmail.com>
2024-06-01 15:15:31 -0400

    still trying to fix permission bits

---
+++ top/build_linux.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# This script is used by the CI/Build machine to build the Linux projects
+#
+# The script ASSUMES that the working directory is the package root
+#
+# usage: build_linux.sh <bldnum>
+#
+
+# setup the environment
+source ./env.sh default
+
+# Build all test linux projects (only 64bit versions)
+# TODO: Currently building debug versions -->For non-debug builds - warnings are generated when using gcc 12 -->FIXME!
+pushd tests
+$NQBP_BIN/other/bob.py -v4 linux  -gb posix64 --bldtime --bldnum $1
+
+# Run unit tests 
+$NQBP_BIN/other/chuck.py -v --match a.out --dir _posix64
+$NQBP_BIN/other/chuck.py -v --match aa.out --dir _posix64
+$NQBP_BIN/other/chuck.py -v --match a.py --dir _posix64
+$NQBP_BIN/other/chuck.py -v --match aa.py --dir _posix64
+
+# Build all "projects/" linux projects (only 64bit versions)
+# TODO: See above issue with GCC 12 -->FIXME!
+popd
+pushd projects
+$NQBP_BIN/other/bob.py -v4 linux  -gb posix64 --bldtime --bldnum $1
+
